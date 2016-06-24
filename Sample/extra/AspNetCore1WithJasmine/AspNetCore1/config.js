eb5ace58d4cb51ef7eb47c4997b163e03a068193
Jeppe Andersen <jeppe@outlook.com>
2016-06-24 09:07:03 +0200

    Added ASP.NET Core 1.0 example with Jasmine

---
+++ Sample/extra/AspNetCore1WithJasmine/AspNetCore1/config.js
@@ -0,0 +1,25 @@
+﻿var path = require("path");
+
+// Folder names for client code (sources, distribution)
+exports.APP_SRC = "src";
+exports.APP_DIST = "wwwroot";
+exports.TEST_DIST = "wwwroot/tests/spec/*.js";
+
+// TypeScript sources
+exports.TS_SOURCES =
+    ["typings/index.d.ts", path.join(exports.APP_SRC, "**/*.ts")]
+
+// External script dependencies (combined into single file)
+exports.SCRIPT_DEPENDENCIES = [
+    "node_modules/systemjs/dist/system.src.js",
+    "node_modules/angular2/bundles/angular2-polyfills.js",
+    "node_modules/rxjs/bundles/Rx.js",
+    "node_modules/angular2/bundles/angular2.dev.js",
+    "node_modules/angular2/bundles/http.dev.js"
+];
+exports.SCRIPT_COMBINED = "scripts.js";
+
+exports.STYLES_DEPENDENCIES = [
+    "node_modules/bootstrap/dist/css/bootstrap.css"
+];
+exports.STYLES_COMBINED = "styles.css";
---
+++ Sample/extra/AspNetCore1WithJasmine/AspNetCore1/src/tests/spec/apptest.spec.ts
@@ -0,0 +1,31 @@
+﻿import 'reflect-metadata';
+import 'zone.js';
+import { AppComponent, IBook } from '../../app/app.component';
+import { Observable } from 'rx';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+
+describe("Books application", () => {
+    describe("shopping cart", () => {
+        it("should contain books when adding to cart",
+            () => {                
+                var sut = new AppComponent();
+                var book: IBook = {
+                    id: 1,
+                    title: "Revenge of the Knight",
+                    description: "Lorem ipsum",
+                    price: 42
+                };
+
+                sut.addToShoppingCart(book);
+
+                expect(sut.cart.length).toBe(1);
+            });
+
+        it("should be empty when nothing is added",
+            () => {
+                var sut = new AppComponent();
+
+                expect(sut.cart.length).toBe(0);
+            });
+    });
+});
